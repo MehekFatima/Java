551a5c495266d204d29a0c0d76187375540ca632
Mehek Fatima <128465778+MehekFatima@users.noreply.github.com>
2023-11-25 01:00:00 +0530

    UserInput.java

--- UserInput.java
+++ UserInput.java
@@ -6,10 +6,12 @@ public class UserInput{
         Scanner sc = new Scanner(System.in);
         String Username = sc.nextLine();
         System.out.println("The username is: "+Username);
+        
         System.out.println("Enter your Age: ");
         int age;
         age = sc.nextInt();
         System.out.println("your age is: " +age);
+        
         System.out.println("Enter your salary: ");
         double salary;
         salary = sc.nextDouble();
