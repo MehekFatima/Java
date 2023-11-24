16d936f6f6431c31710ca18d4d2be66a738b33cc
Mehek Fatima <128465778+MehekFatima@users.noreply.github.com>
2023-11-25 00:57:39 +0530

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class UserInput{
+    public static void main(String[] args) {
+        System.out.println("Enter the username: ");
+        Scanner sc = new Scanner(System.in);
+        String Username = sc.nextLine();
+        System.out.println("The username is: "+Username);
+        System.out.println("Enter your Age: ");
+        int age;
+        age = sc.nextInt();
+        System.out.println("your age is: " +age);
+        System.out.println("Enter your salary: ");
+        double salary;
+        salary = sc.nextDouble();
+        System.out.println("The salary is:" +salary);
+        
+    }
+}
