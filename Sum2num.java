d977cfcf6d26e016c382cb2f8651c0863c946194
Mehek Fatima <128465778+MehekFatima@users.noreply.github.com>
2023-11-25 13:24:26 +0530

    Add files via upload

---
+++ Sum2num.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class Sum2num
+{
+  public static void main (String[]args)
+  {
+    System.out.println ("Enter num1: ");
+    Scanner sc = new Scanner (System.in);
+    int num1 = sc.nextInt ();
+      System.out.println ("Enter num2: ");
+    int num2 = sc.nextInt ();
+    int sum = num1 + num2;
+      System.out.println ("The sum is: " + sum);
+
+
+  }
+}
