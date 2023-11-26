e1d0e6c490352417a554690e331c8239346cc17e
Mehek Fatima <128465778+MehekFatima@users.noreply.github.com>
2023-11-26 23:21:45 +0530

    InvertedHP.java

--- InvertedHP.java
+++ InvertedHP.java
@@ -5,7 +5,7 @@ public class InvertedHP
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter value for n(rows):");
 		int n = sc.nextInt();
-		for(int i=n;i<=n;i--){
+		for(int i=n;i>=1;i--){
 		    for(int j=1;j<= i;j++){
 		        System.out.print("*");
 		    }
