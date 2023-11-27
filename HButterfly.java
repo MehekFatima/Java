16b7b61340d2a84860dccccfa39337ae2a7db967
Mehek Fatima <128465778+MehekFatima@users.noreply.github.com>
2023-11-27 11:30:53 +0530

    Add files via upload
    To print a hollow Butterfly.

---
+++ HButterfly.java
@@ -0,0 +1,61 @@
+import java.util.*;
+public class HButterfly{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int n = sc.nextInt();
+        //upper
+        for(int i = 1;i<=n;i++){
+            //1st
+            for(int j=1;j<=i;j++){
+                if(i==1||j==1||i==j){
+		            System.out.print("*");
+		        }
+		        else{
+		            System.out.print(" ");
+		        }
+            }
+            //spaces
+            for(int j=1;j<=(n-i)*2;j++){
+                System.out.print(" ");
+            }
+            //2nd part
+            for(int j=1;j<=i;j++){
+                if(i==1||j==1||i==j){
+		            System.out.print("*");
+		        }
+		        else{
+		            System.out.print(" ");
+		        }
+            }
+            System.out.println();
+        }
+        //lower
+        for(int i = n;i>=1;i--){
+            //1st
+            for(int j=1;j<=i;j++){
+                if(i==1||j==1||i==j){
+		            System.out.print("*");
+		        }
+		        else{
+		            System.out.print(" ");
+		        }
+            }
+            //spaces
+            for(int j=1;j<=(n-i)*2;j++){
+                System.out.print(" ");
+            }
+            //2nd part
+            for(int j=1;j<=i;j++){
+                if(i==1||j==1||i==j){
+		            System.out.print("*");
+		        }
+		        else{
+		            System.out.print(" ");
+		        }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
