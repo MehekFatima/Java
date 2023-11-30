66e66837bba6f14a6297ed3f3f48a09c708f5863
Mehek Fatima <128465778+MehekFatima@users.noreply.github.com>
2023-11-30 22:46:00 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,13 @@
+
+public class Array
+{
+	public static void main(String[] args) {
+	    int[] marks = new int[3];
+	    marks[0]=85;
+	    marks[1] = 90;
+	    marks[2] =95;
+	    for(int i =0;i<3;i++){
+		System.out.println(marks[i]);
+	    }
+	}
+}
